66eb65245ff5b84caae4fd17ca8c897e16c747cf
Manjinder Singh <49171515+jinder1s@users.noreply.github.com>
2020-03-16 14:59:07 -0400

    Adding repo health state script code (#1)
    * Standardizing repo

--- setup.py
+++ setup.py
@@ -10,6 +10,33 @@ def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
     return codecs.open(file_path, encoding='utf-8').read()
 
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+
+    Returns:
+        list: Requirements file relative path strings
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line.strip())
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement.
+
+    Returns:
+        bool: True if the line is not blank, a comment, a URL, or an included file
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.rst')).read()
 
 setup(
     name='pytest-opynions',
@@ -18,30 +45,26 @@ setup(
     author_email='msingh@edx.org',
     maintainer='Manjinder Singh',
     maintainer_email='msingh@edx.org',
-    license='MIT',
     url='https://github.com/jinder1s/pytest-opynions',
     description='A simple plugin to use with pytest',
     long_description=read('README.rst'),
     py_modules=['pytest_opynions'],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['pytest>=3.5.0'],
+    python_requires=">=3.5",
+    install_requires=load_requirements('requirements/base.in'),
+    zip_safe=False,
+    keywords='Django edx',
+    license="Apache Software License 2.0",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={
         'pytest11': [
