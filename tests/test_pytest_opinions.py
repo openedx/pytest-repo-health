071bb5fea2e1281357a201d1482cbc992f8e0991
jinder1s <msingh@edx.org>
2020-03-20 09:46:20 -0400

    Fixeds tests

--- tests/test_pytest_opinions.py
+++ tests/test_pytest_opinions.py
@@ -7,32 +7,32 @@ import os
 import yaml
 
 
-def test_arguments_in_help(misc_testdir):
-    res = misc_testdir.runpytest('--help')
+def test_arguments_in_help(testdir):
+    res = testdir.runpytest('--help')
     res.stdout.fnmatch_lines_random([
-        '*repo-health-check*',
+        '*opynions*',
         '*output-path*',
         '*repo-path*',
     ])
 
-def test_no_report(misc_testdir):
+def test_no_report(testdir):
     """
     Check to make sure report is not generated without --repo-health-check
     """
-    misc_testdir.runpytest()
-    assert not (misc_testdir.tmpdir / 'repo_state.yaml').exists()
+    testdir.runpytest()
+    assert not (testdir.tmpdir / 'repo_state.yaml').exists()
 
 
-def test_create_report(misc_testdir):
-    res = misc_testdir.runpytest('--repo-health-check')
-    assert (misc_testdir.tmpdir / 'repo_state.yaml').exists()
+def test_create_report(testdir):
+    res = testdir.runpytest('--opynions')
+    assert (testdir.tmpdir / 'repo_state.yaml').exists()
 
-def test_report_content_has_required_module_keys(misc_testdir):
+def test_report_content_has_required_module_keys(testdir):
     """
     eventuall this test should be removed and replaced by seperate tests for each check module
     """
-    res = misc_testdir.runpytest('--repo-health-check')
-    yaml_lines = (misc_testdir.tmpdir / 'repo_state.yaml').read()
+    res = testdir.runpytest('--opynions')
+    yaml_lines = (testdir.tmpdir / 'repo_state.yaml').read()
     output_dict = yaml.safe_load(yaml_lines)
     assert 'makefile' in output_dict.keys()
     assert 'openedx_yaml' in output_dict.keys()
