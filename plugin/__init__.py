3c7a00f4e20f9aa56bfd145a562195b8e81de74c
jinder1s <msingh@edx.org>
2020-04-08 15:49:38 -0400

    Fixed docs and renamed directory

---
+++ plugin/__init__.py
@@ -0,0 +1,8 @@
+"""
+Plugin configures pytest to run repo health checks on a code repository.
+Plugin takes care of gathering checks, runing checks,
+and outputing report based on data gathered during checks.
+"""
+
+
+__version__ = "0.1.0"
