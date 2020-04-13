baa5090985e3e9e1051928a064a6186749483a89
jinder1s <msingh@edx.org>
2020-04-13 09:25:44 -0400

    Added Decorator to add docs for checks
    -Renamed repo to allow checks to import decorator

--- plugin/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Plugin configures pytest to run repo health checks on a code repository.
-Plugin takes care of gathering checks, runing checks,
-and outputing report based on data gathered during checks.
-"""
-
-
-__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def is_requirement(line):
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.rst')).read()
-VERSION = get_version('plugin', '__init__.py')
+VERSION = get_version('pytest_repo_health', '__init__.py')
 
 setup(
     name='pytest-repo-health',
@@ -62,7 +62,7 @@ setup(
     url='https://github.com/jinder1s/pytest-repo-health',
     description='A simple plugin to use with pytest',
     long_description=read('README.rst'),
-    py_modules=['plugin.plugin'],
+    py_modules=['pytest_repo_health.plugin'],
     python_requires=">=3.5",
     install_requires=load_requirements('requirements/base.in'),
     zip_safe=False,
@@ -82,7 +82,7 @@ setup(
     ],
     entry_points={
         'pytest11': [
-            'plugin = plugin.plugin',
+            'plugin = pytest_repo_health.plugin',
         ],
     },
 )
