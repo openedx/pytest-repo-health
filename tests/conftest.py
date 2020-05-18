4b3bee461b69aa595caab36354892adc3e023ca1
jinder1s <msingh@edx.org>
2020-05-18 12:12:35 -0400

    Cleaning up testing code

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@
 conftest used to run tests on pytest-repo-health plugin
 """
 import pytest
-import pdb
 
 pytest_plugins = 'pytester'
 
@@ -11,9 +10,3 @@ FILE = """
 def check_test_collection(all_results):
     all_results["Yes"] = "NOOOO"
 """
-
-@pytest.fixture
-def misc_testdir(testdir):
-    testdir.mkdir("repo_health")
-    testdir.makefile('.py',**{"/repo_health/blah":FILE})
-    return testdir
