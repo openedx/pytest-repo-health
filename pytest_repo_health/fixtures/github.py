ae984428c623bf9c6ca1346b6294a4e44781a961
Aarif <mraarif@outlook.com>
2020-10-09 17:14:24 +0500

    added error logging to replace raising exceptions

--- pytest_repo_health/fixtures/github.py
+++ pytest_repo_health/fixtures/github.py
@@ -1,8 +1,10 @@
 """
 Fixtures for getting information about a repository hosted on GitHub.
 """
+import logging
 import os
 import re
+from collections import namedtuple
 
 import pytest
 try:
@@ -15,6 +17,12 @@ from github.errors import GitHubError
 
 URL_PATTERN = r"github.com[/:](?P<org_name>[^/]+)/(?P<repo_name>[^/]+).git"
 
+logger = logging.getLogger(__name__)
+
+GITHUB_TOKEN_MISSING_ERROR_MESSAGE = "Error. No GITHUB_TOKEN environment variable found. To use any of the GitHub " \
+                                     "fixtures, you must set the GITHUB_TOKEN environment variable to contain a " \
+                                     "GitHub personal access token."
+
 
 @pytest.fixture(scope="session")
 def github_client():
@@ -22,12 +30,15 @@ def github_client():
     A fixture to initialize a GitHub API client, using the personal access token
     obtained from the GITHUB_TOKEN environment variable.
     """
+    GitHubClient = namedtuple('GitHubClient', 'object message')
+
     try:
         token = os.environ["GITHUB_TOKEN"]
     except KeyError:
-        raise Exception("To use any of the GitHub fixtures, you must set the GITHUB_TOKEN environment variable "
-                        "to contain a GitHub personal access token.")
-    return Client(token)
+        logger.error(GITHUB_TOKEN_MISSING_ERROR_MESSAGE)
+        return GitHubClient(None, GITHUB_TOKEN_MISSING_ERROR_MESSAGE)
+
+    return GitHubClient(Client(token), None)
 
 
 @pytest.fixture
@@ -39,17 +50,24 @@ async def github_repo(git_origin_url, github_client, loop):  # pylint: disable=r
     GitHub (such as a network failure or GitHub internal error).
     """
     if git_origin_url is None:
-        # There isn't an origin for this repository or directory
-        raise Exception("There isn't an origin for this repository or directory")
+        logger.error("Error. No origin found. There isn't an origin for this repository or directory")
 
     match = re.search(URL_PATTERN, git_origin_url)
     if match is None:
-        # The origin isn't hosted on GitHub (might be GitLab, Bitbucket, etc.)
-        raise Exception("The origin isn't hosted on GitHub")
+        logger.error("Invalid origin. Must be a GitHub origin. The origin isn't hosted on GitHub")
+        return None
 
     org_name = match.group("org_name")
     repo_name = match.group("repo_name")
+    GitHubClient = namedtuple('GitHubClient', 'object message')
+
+    if github_client.object is None:
+        logger.error(github_client.message)
+        return GitHubClient(None, github_client.message)
+
     try:
-        return await github_client.fetch_repository(org_name, repo_name)
+        return GitHubClient(await github_client.fetch_repository(org_name, repo_name), None)
     except GitHubError as e:
-        raise Exception('GithubAPI throws an error, "{0}"'.format(e))
+        error_message = 'An error occurred while fetching the repository. GitHub API threw an error, "{0}"'.format(e)
+        logger.error(error_message, exc_info=e)
+        return GitHubClient(None, error_message)
