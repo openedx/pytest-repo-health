a335bf87199e71eea9bb8d6b5e9d485daccff860
awais qureshi <awais.qureshi@arbisoft.com>
2024-04-15 12:26:53 +0500

    feat: Adding python3.11 support.

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points={
         'pytest11': [
